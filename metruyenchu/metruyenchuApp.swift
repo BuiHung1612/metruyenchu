f691dd95c80dd05ff9abec78448fbb8c42743cd3
HungBui1612 <hungbv@osp.com>
2024-05-10 08:58:52 +0700

    Initial Commit

---
+++ metruyenchu/metruyenchuApp.swift
@@ -0,0 +1,17 @@
+//
+//  metruyenchuApp.swift
+//  metruyenchu
+//
+//  Created by HungBui on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct metruyenchuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
